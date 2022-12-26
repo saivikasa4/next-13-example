07c8e8db42186816d3b1f1b861d8fac4deb3402f
ruthvik <ruthv1k@proton.me>
2022-12-26 11:46:54 +0530

    Add tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import styles from "./page.module.css"
-
 export default function Home() {
-  return <div className={styles.heading}>Welcome</div>
+  return <div className="text-3xl font-bold">Welcome</div>
 }
