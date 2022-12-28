8bde133e8f7c13e0310f23f4173f932379698a41
ruthvik <ruthv1k@proton.me>
2022-12-28 19:42:54 +0530

    Add heroicons
    - Updated dashboard home

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,6 +3,7 @@
 import logout from "@/lib/logout"
 import Link from "next/link"
 import { useRouter } from "next/navigation"
+import { PlusIcon } from "@heroicons/react/24/solid"
 
 function Dashboard() {
   const router = useRouter()
@@ -15,13 +16,27 @@ function Dashboard() {
   }
   return (
     <div>
-      <h1 className="text-xl md:text-3xl font-bold mb-4">You are logged in</h1>
-      <Link href="/dashboard/profile" className="text-blue-500 text-xl mr-4">
-        My Profile
-      </Link>
-      <button onClick={handleLogout} className="text-blue-500 text-xl">
-        Logout
-      </button>
+      <header className="mb-10">
+        <h1 className="text-xl md:text-3xl font-bold mb-4">
+          You are logged in
+        </h1>
+        <Link href="/dashboard/profile" className="text-blue-500 text-xl mr-4">
+          My Profile
+        </Link>
+        <button onClick={handleLogout} className="text-blue-500 text-xl">
+          Logout
+        </button>
+      </header>
+
+      <section>
+        <Link
+          href="/dashboard/post/create"
+          className="md:w-[160px] md:h-[160px] font-medium hover:bg-blue-500 transition-all duration-150 ease-in-out hover:text-white dark:hover:text-black rounded-xl flex md:text-xl flex-col items-center justify-center text-blue-500 border border-blue-500"
+        >
+          <PlusIcon className="w-10 h-10 mb-1 block" />
+          Create Post
+        </Link>
+      </section>
     </div>
   )
 }
